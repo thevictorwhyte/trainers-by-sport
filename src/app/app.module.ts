fa3705a7d1f9af42d256e9da24594dd07813e1fa
Victor Whyte <victordavidwhyte@gmail.com>
2023-09-30 22:44:57 +0100

    feat: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/ui/button/button.component';
+import { TrainerCardComponent } from './components/cards/trainer-card/trainer-card.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    TrainerCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/cards/trainer-card/trainer-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-card',
+  templateUrl: './trainer-card.component.html',
+  styleUrls: ['./trainer-card.component.scss']
+})
+export class TrainerCardComponent {
+
+}
